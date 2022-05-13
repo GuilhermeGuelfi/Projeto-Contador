b535e46a70c932a1f4fc06edbf9c1d6f23020ab8
GuilhermeGuelfi <98917451+GuilhermeGuelfi@users.noreply.github.com>
2022-05-13 17:29:36 -0400

    Atualizado

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
 var cont = window.document.getElementById('contador')
 var res = 0
 
-function ok() {
+function aumentar() { // Para somar valores
     var passos = window.document.getElementById('txtpassos').value
     passos = Number(passos)
-}
+    if (document.getElementById('txtpassos').value.length == 0) { 
+        passos == 0 ? res += 1 : res += passos
+        cont.innerHTML = res 
+        // Caso o usuário não escolha nenhum valor no input
 
-function aumentar() {
-    var passos = window.document.getElementById('txtpassos').value
-    passos = Number(passos)
-    res += passos
-    cont.innerHTML = res
+    } else if (document.getElementById('txtpassos').value <1) {
+        window.alert('Valores menores ou iguais a "0" são inválidos')
+        // Caso o usuário escolha valores menores que 1
+
+    } else {
+        passos == 0 ? res += 1 : res += passos
+        cont.innerHTML = res
+        // Caso o usuário escolha valores maiores que 1
+    }
 }
 
 function diminuir() {
     var passos = window.document.getElementById('txtpassos').value
     passos = Number(passos)
-    res -= passos
-    cont.innerHTML = res
+    if (document.getElementById('txtpassos').value.length == 0) {
+        passos == 0 ? res -= 1 : res -= passos
+        cont.innerHTML = res
+        // Caso o usuário não escolha um valor no input
+        
+    } else if (document.getElementById('txtpassos').value <1) {
+        window.alert('Valores menores ou iguais a "0" são inválidos')
+        // Caso o usuário escolha valores menores que 1
+
+    } else {
+        passos == 0 ? res -= 1 : res -= passos
+        cont.innerHTML = res
+        // Caso o usuário escolha valores maiores que 1
+    }
 }
    
 function resetar() {
     res = 0
     cont.innerHTML = res
+    // Botão de resetar -> Volta para 0
 }
